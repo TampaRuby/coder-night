02520f29b3942a4b01af2ecc28d0a6d1075ca49d
Jason L Perry <jasper@ambethia.com>
2012-05-07 22:00:34 -0400

    Initial import.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CoderNight::Application.config.session_store :cookie_store, key: '_coder_night_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CoderNight::Application.config.session_store :active_record_store
