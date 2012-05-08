64ef80d856a3c1b2f3989a0420fbec082f2a760f
Jason L Perry <jasper@ambethia.com>
2012-05-08 15:14:26 -0400

    Remove Skim reference

--- config/initializers/assets.rb
+++
@@ -1,6 +0,0 @@
-Skim::Engine.default_options[:use_asset] = true
-
-Rails.application.assets.context_class.instance_eval do
-  include ActionView::Helpers::UrlHelper
-  include Rails.application.routes.url_helpers
-end
