fcf7600959e9c6ec95b44fd995fc5eb8555b13df
Jason L Perry <jasper@ambethia.com>
2012-05-11 19:13:52 -0400

    skip initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,9 @@ module CoderNight
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar#troubleshooting
+    config.assets.initialize_on_precompile = false
   end
 end
 
